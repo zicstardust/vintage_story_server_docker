34bcbe4e7c1c85434b77c528d6ca7803c023192d
Zic <git@zicstardust.com>
2025-08-15 15:45:54 -0400

    better output

--- download_dotnet.sh
+++ download_dotnet.sh
@@ -6,12 +6,16 @@ set -e
 
 
 if [ "$DOTNET_VERSION" == "mono" ]; then
-    apt-get update && apt-get install -y mono-complete
+    echo "Downloading mono..."
+    apt-get update  &> /dev/null
+    apt-get install -y mono-complete &> /dev/null
 else
-    apt-get update && apt-get install -y libc6 libgcc-s1 libgssapi-krb5-2 libicu72 libssl3 libstdc++6 zlib1g
+    echo "Downloading .NET Runtime ${DOTNET_VERSION}..."
+    apt-get update &> /dev/null
+    apt-get install -y libc6 libgcc-s1 libgssapi-krb5-2 libicu72 libssl3 libstdc++6 zlib1g  &> /dev/null
     mkdir -p dotnet_temp
     cd dotnet_temp
-    wget https://builds.dotnet.microsoft.com/dotnet/Runtime/${DOTNET_VERSION}/dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz
+    wget https://builds.dotnet.microsoft.com/dotnet/Runtime/${DOTNET_VERSION}/dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz  &> /dev/null
     tar xf dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz
     mv /app/dotnet_temp/* /usr/local/bin
     cd /app
