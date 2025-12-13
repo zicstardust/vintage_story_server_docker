d81e882092d7e6319296f649ee9e2b0dc2a462c4
Zic <git@zicstardust.com>
2025-12-13 08:14:58 -0400

    add DOTNET_ROOT environment variables

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -f "/app/VintagestoryServer.dll" ]; then
-    dotnet /app/VintagestoryServer.dll --dataPath /data
+    DOTNET_ROOT="/opt/dotnet" dotnet /app/VintagestoryServer.dll --dataPath /data
 elif [ -f "/app/vintagestory/VintagestoryServer.exe" ]; then
     mono /app/vintagestory/VintagestoryServer.exe --dataPath /data
 else
