605de2e30d1ca3fa665a8bac13429b65a15cde16
Zic <git@zicstardust.com>
2025-12-16 17:59:45 -0400

    remove output wget from download_dotnet.sh

--- download_dotnet.sh
+++ download_dotnet.sh
@@ -15,7 +15,7 @@ else
     apt-get install -y libc6 libgcc-s1 libgssapi-krb5-2 libicu76 libssl3 libstdc++6 zlib1g  &> /dev/null
     mkdir -p /opt/dotnet
     cd /opt/dotnet
-    wget https://builds.dotnet.microsoft.com/dotnet/Runtime/${DOTNET_VERSION}/dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz  &> /dev/null
+    wget -q https://builds.dotnet.microsoft.com/dotnet/Runtime/${DOTNET_VERSION}/dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz
     tar xf dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz
     rm -f dotnet-runtime-${DOTNET_VERSION}-linux-x64.tar.gz
     ln -s /opt/dotnet/dotnet /usr/local/bin/dotnet
